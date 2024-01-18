a5340ee3b0324df4ec6596c5ab6118f5aa9a7a06
CK-codemax <ckcodemax@gmail.com>
2024-01-19 00:44:06 +0100

    completely built the selector page

--- app/components/Selector.jsx
+++ app/components/Selector.jsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react"
 import LineBreak from "./LineBreak"
+import Logo from "./Logo"
+import Image from "next/image"
 
 export default function Selector() {
     const [opened, setOpened] = useState('hulu')
@@ -18,9 +20,9 @@ export default function Selector() {
 
        <LineBreak />
 
-       <div className="text-white">
+       
         {opened === 'hulu' && (
-            <div className="mt-5 flex space-x-6 items-end ml-[35%]">
+            <div className="mt-5 flex text-white space-x-6 items-end ml-[35%]">
                 <div className="flex flex-col items-center">
                     <p className="text-black mb-5 bg-[#1ce783] rounded-full text-[10px] font-bold px-4 py-1 text-center inline-block">MOST POPULAR</p>
                     <p className="text-[10px] font-bold mb-1">30 DAY FREE TRIAL</p>
@@ -33,9 +35,83 @@ export default function Selector() {
                     <p className="font-bold mb-3">Hulu (No Ads)</p>
                     <p className="cursor-pointer py-2 text-center font-bold hover:bg-gray-300 transition-colors duration-300 ease-in-out text-black bg-white w-[300px] rounded-md">$17.99 / MONTH</p>
                 </div>
-            </div>
+         </div>
         )}
-       </div>
+
+        {opened === 'disney-bundle' && (
+          <div className="-translate-y-2 flex text-white items-end ml-[25%] -space-x-20">
+          <div className="flex flex-col justify-center items-center">
+          <div className="flex scale-75 items-center translate-y-5 sm:px-[50px]">
+      <Logo />
+          <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+src="/disney-logo.png" width={200} height={100} />
+
+      </div>
+              <p className="font-bold mb-3">Disney Bundle Duo Basic</p>
+              <p className="cursor-pointer py-2 text-center font-bold hover:bg-gray-300 transition-colors duration-300 ease-in-out text-black bg-white w-[230px] rounded-md">$9.99 / MONTH</p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+          <div className="flex scale-[62%] items-center translate-y-5">
+      <Logo />
+          <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+src="/disney-logo.png" width={200} height={100} />
+
+          <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+src="/espn-logo.png" width={200} height={100} />
+
+      </div>
+              <p className="font-bold mb-3">Disney Bundle Trio Basic</p>
+              <p className="cursor-pointer py-2 text-center font-bold hover:bg-gray-300 transition-colors duration-300 ease-in-out text-black bg-white w-[230px] rounded-md">$14.99 / MONTH</p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+          <div className="flex scale-[62%] items-center translate-y-5">
+      <Logo />
+          <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+src="/disney-logo.png" width={200} height={100} />
+
+          <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+src="/espn-logo.png" width={200} height={100} />
+
+      </div>
+              <p className="font-bold mb-3">Disney Bundle Trio Premium</p>
+              <p className="cursor-pointer py-2 text-center font-bold hover:bg-gray-300 transition-colors duration-300 ease-in-out text-black bg-white w-[230px] rounded-md">$24.99 / MONTH</p>
+          </div>
+   </div>
+        )}
+
+{opened === 'live-tv' && (
+            <div className="-translate-y-2 flex text-white items-end ml-[35%]">
+                <div className="flex flex-col items-center">
+                <div className="flex space-x-1 scale-[62%] items-center translate-y-5">
+            <Logo />
+                <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+    src="/disney-logo.png" width={200} height={100} />
+    
+                <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+    src="/espn-logo.png" width={200} height={100} />
+    
+            </div>
+                    <p className="font-bold mb-3">Hulu + Live TV</p>
+                    <p className="cursor-pointer py-2 text-center font-bold hover:bg-gray-300 transition-colors duration-300 ease-in-out text-black bg-white w-[300px] rounded-md">$76.99 / MONTH</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                <div className="flex space-x-1 scale-[62%] items-center translate-y-5">
+            <Logo />
+                <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+    src="/disney-logo.png" width={200} height={100} />
+    
+                <Image className="object-contain w-[70px] sm:w-[100px] h-auto" alt='hulu-logo'
+    src="/espn-logo.png" width={200} height={100} />
+    
+            </div>
+                    <p className="font-bold mb-3">Hulu (No Ads) + Live TV</p>
+                    <p className="cursor-pointer py-2 text-center font-bold hover:bg-gray-300 transition-colors duration-300 ease-in-out text-black bg-white w-[300px] rounded-md">$89.99 / MONTH</p>
+                </div>
+         </div>
+        )}
   </div>
    
   )
