1f70b9fd9fac42a43f0d4724cebc80028a711f0b
CK-codemax <ckcodemax@gmail.com>
2024-01-17 17:31:32 -0800

    Started building the middle component

---
+++ app/components/Middle.jsx
@@ -0,0 +1,21 @@
+export default function Middle() {
+  return (
+  
+  <>
+   <div className="w-full pt-8 pb-6 bg-gradient-to-b from-[#1ce783] to-gray-900 ">
+        <p className="text-white tracking-widest text-l font-bold uppercase mb-8 text-center">build the plan that's right for you</p>
+        <p className="text-white cursor-pointer hover:bg-gray-200 hover:text-gray-800 mx-auto text-center text-sm font-bold tracking-widest uppercase py-2 w-[300px] bg-transparent border-2 border-gray-200 rounded-md">start building</p>
+    </div>
+
+    <div className="bg-black pb-10 pt-28">
+         <p className="text-[#1ce783] mb-4 text-center text-xs font-semibold uppercase tracking-widest">included in all your plans</p>
+
+         <p className="text-white mb-4 font-bold text-[40px] text-center">All The TV You Love</p>
+
+         <p className="text-white text-wrap w-[800px] text-center mx-auto">Watch full seasons of exclusive streaming series, current-season episodes, hit movies, Hulu Originals, kids shows, and more.</p>
+
+    </div>
+  </>
+   
+  )
+}
