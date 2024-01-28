a1d367e0daabd9e9307c0ab87d248c7b1121a362
CK-codemax <ckcodemax@gmail.com>
2024-01-28 07:38:14 +0100

    still fixing ui

--- app/components/ImageSelector.jsx
+++ app/components/ImageSelector.jsx
@@ -5,15 +5,9 @@ export default function ImageSelector() {
 
   const [opened, setOpened] = useState('live-sports')
  
- const bgImage = opened === 'live-sports' && 'https://cnbl-cdn.bamgrid.com/assets/3ab6ce86baaf4225b072c58fa9497ec949c0c9e284b3958ecda1429b7f3744f9/original' || 
-  opened === 'breaking-news' && 'https://cnbl-cdn.bamgrid.com/assets/4a3aa8e2de730150aba6be81a274da9d447c513fbb0a788dad007b5fe1e0877a/original' || 
-  opened === 'biggest-events' && 'https://cnbl-cdn.bamgrid.com/assets/1cfd5743e36004288a408e977a0a9e44b57668b2bfdc525956772ce9d8769288/original'
-  
-  const style = {
-    backgroundImage : `linear-gradient(rgba(0, 0, 0, 0.8) -39.59%, rgba(0, 0, 0, 0) 117.14%), url(${bgImage})`,
-  }
+ 
   return (<div className="relative w-full min-h-screen h-[800px] bg-[#0b0c0f] pt-5 pb-20">
-        <div className="absolute z-10 top-[75px] left-[50%] -translate-x-[50%] mx-auto xl:top-[200px] xl:left-24 flex space-x-6">
+        <div className="absolute z-10 top-[75px] left-[50%] -translate-x-[50%] xl:-translate-x-[0] mx-auto xl:top-[200px] xl:left-24 flex space-x-6">
             <p onClick={() => setOpened('live-sports')} className={` uppercase cursor-pointer text-nowrap font-bold text-xs sm:text-sm  ${opened === 'live-sports' ? 'text-white underline underline-offset-[16px] decoration-4' : 'hover:text-gray-600 text-gray-500'}`}>Live Sports</p>
             
             <p onClick={() => setOpened('breaking-news')} className={` uppercase cursor-pointer text-nowrap font-bold text-xs sm:text-sm   ${opened === 'breaking-news' ? 'text-white underline underline-offset-[16px] decoration-4' : 'hover:text-gray-600 text-gray-500'}`}>Breaking News</p>
@@ -21,39 +15,39 @@ export default function ImageSelector() {
             <p onClick={() => setOpened('biggest-events')} className={` uppercase cursor-pointer text-nowrap font-bold text-xs sm:text-sm   ${opened === 'biggest-events' ? 'text-white underline underline-offset-[16px] decoration-4' : 'hover:text-gray-600 text-gray-500'}`}>Biggest Events</p>
         
         </div>
-         {/* <div style={style} className={`absolute bg-cover w-full min-h-screen h-[702px] bg-no-repeat`}/>
-         {opened === 'live-sports' && (<div className="absolute left-24 top-[280px]">
-          <p className="text-white mb-4 font-bold text-[40px] text-left">Live Sports</p>
-          <p className="text-white text-wrap w-[550px] text-left mx-auto mb-4">Catch your games at home or on the go. Stream live games from major college and pro leagues including the NCAA®, NBA, NHL, NFL, and more. Includes top local and national sports networks like ABC, CBS Sports Network, ESPN, FOX, FS1, TNT, NFL Network and more.</p>
+         <div className={`absolute ${opened === 'live-sports' && 'live-sports'} ${opened === 'biggest-events' && 'biggest-events'} ${opened === 'breaking-news' && 'breaking-news'} bg-cover w-full min-h-screen h-[702px] bg-no-repeat`}/>
+         {opened === 'live-sports' && (<div className={`absolute ${opened === 'live-sports' ? '-translate-y-10 transition-transform duration-1000 ease-out' : 'translate-y-0'} w-full top-[180px] left-[50%] -translate-x-[50%] xl:-translate-x-[0] xl:left-24 xl:top-[280px]`}>
+          <p className="text-white mb-4 font-bold text-center text-[30px] sm:text-[35px] xl:text-[40px] xl:text-left">Live Sports</p>
+          <p className="text-white text-wrap text-center w-[90%] sm:w-[75%] xl:w-[550px] xl:mx-0 xl:text-left mx-auto mb-4">Catch your games at home or on the go. Stream live games from major college and pro leagues including the NCAA®, NBA, NHL, NFL, and more. Includes top local and national sports networks like ABC, CBS Sports Network, ESPN, FOX, FS1, TNT, NFL Network and more.</p>
           
 
           
-         <p className="text-gray-400 mx-auto mb-5 text-wrap text-left w-[550px] block text-xs">
+         <p className="text-gray-400 text-center w-[90%] sm:w-[75%] mx-auto mb-5 text-wrap xl:mx-0 xl:text-left xl:w-[550px] block text-xs">
          Live TV plan required. Regional restrictions, blackouts and additional terms apply. <span className="underline cursor-pointer">See details</span>
          </p>
          </div>)}
 
-         {opened === 'breaking-news' && (<div className="absolute left-24 top-[280px]">
-          <p className="text-white mb-4 font-bold text-[40px] text-left">Breaking News</p>
-          <p className="text-white text-wrap w-[550px] text-left mx-auto mb-4">Keep pace with what's going on locally and globally with trusted opinions from all the top news networks.</p>
+        {opened === 'breaking-news' && (<div className="absolute w-full top-[180px] left-[50%] -translate-x-[50%] xl:-translate-x-[0] xl:left-24 xl:top-[280px]">
+          <p className="text-white mb-4 font-bold text-center text-[30px] sm:text-[35px] xl:text-[40px] xl:text-left">Breaking News</p>
+          <p className="text-white text-wrap text-center w-[90%] sm:w-[75%] xl:w-[550px] xl:text-left mx-auto xl:mx-0 mb-4">Keep pace with what's going on locally and globally with trusted opinions from all the top news networks.</p>
           
 
           
-         <p className="text-gray-400 mx-auto mb-5 text-wrap text-left w-[550px] block text-xs">
+         <p className="text-gray-400 text-center w-[90%] sm:w-[75%] mx-auto mb-5 text-wrap xl:mx-0 xl:text-left xl:w-[550px] block text-xs">
          Live TV plan required. Regional restrictions, blackouts and additional terms apply. <span className="underline cursor-pointer">See details</span>
          </p>
          </div>)}
 
-         {opened === 'biggest-events' && (<div className="absolute left-24 top-[280px]">
-          <p className="text-white mb-4 font-bold text-[40px] text-left">Biggest Events</p>
-          <p className="text-white text-wrap w-[550px] text-left mx-auto mb-4">Spectacular, can't-miss moments like the Olympics, Grammys®, Oscars®, Emmys®, and more.</p>
+         {opened === 'biggest-events' && (<div className="absolute w-full top-[180px] left-[50%] -translate-x-[50%] xl:-translate-x-[0] xl:left-24 xl:top-[280px]">
+          <p className="text-white mb-4 font-bold text-center text-[30px] sm:text-[35px] xl:text-[40px] xl:text-left">Biggest Events</p>
+          <p className="text-white text-wrap text-center w-[90%] sm:w-[75%] xl:w-[550px] xl:mx-0 xl:text-left mx-auto mb-4">Spectacular, can't-miss moments like the Olympics, Grammys®, Oscars®, Emmys®, and more.</p>
           
 
           
-         <p className="text-gray-400 mx-auto mb-5 text-wrap text-left w-[550px] block text-xs">
+         <p className="text-gray-400 text-center w-[90%] sm:w-[75%] mx-auto mb-5 text-wrap xl:mx-0 xl:text-left xl:w-[550px] block text-xs">
          Live TV plan required. Regional restrictions, blackouts and additional terms apply. <span className="underline cursor-pointer">See details</span>
          </p>
-         </div>)} */}
+         </div>)} 
   </div>
    
   )
